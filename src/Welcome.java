2ac6685d0d79b9b6b69190667a4ba83e91861cc1
prajwaldhage <psdhage1@gmail.com>
2024-06-27 19:16:16 +0530

    Initial Commit

---
+++ src/Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    static void title()
+    {
+        System.out.println("Welcome to ABC Bank ATM\n");
+        System.out.println("Please Enter Your A/c Number:");
+    }
+}
